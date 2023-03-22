1acfdb0eb0102e0bff4fbedf2d1fcc5cd3a48781
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-03-22 12:48:30 +0200

    first

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-import stremlit as st
+import streamlit as st
+import folium
+import pandas as pd
+import requests
+import geopandas 
+import shapely.geometry
 
 st.write("Live Map Komy")
+
+
+geodf = geopandas.read_file('testgeo.geojson')
+st.write(geodf)
