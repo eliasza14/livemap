623b1a0d6a3e333c6f7ad592613c7095fb0df7a9
A-PALIOS <paliosadonis6@gmail.com>
2023-03-28 15:42:12 +0300

    Antonis Commit

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ import  streamlit.components.v1
 from folium.plugins import Fullscreen
 
 
-APP_TITLE = 'Κ.Ο.Μ.Υ. 2.1.2 Χάρτης'
+APP_TITLE = 'Κ.Ο.Μ.Υ. 2.1.3 Χάρτης'
 APP_SUB_TITLE = 'by CMT Prooptiki'
 
 
